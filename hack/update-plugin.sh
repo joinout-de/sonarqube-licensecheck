03f3d09c2542857500ccffc111bfebde253bcc1c
Christoph "criztovyl" Schulz <git@criztovyl.space>
2023-03-12 18:00:56 +0100

    Update version
    for sonarqube "New Code" window

--- hack/update-plugin.sh
+++ hack/update-plugin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-podman cp target/sonarqube-licensecheck-plugin-5.1.0-SNAPSHOT.jar sonarqube:/opt/sonarqube/extensions/plugins/
+podman cp target/sonarqube-licensecheck-plugin-*-SNAPSHOT.jar sonarqube:/opt/sonarqube/extensions/plugins/
 podman stop sonarqube
 podman start sonarqube
